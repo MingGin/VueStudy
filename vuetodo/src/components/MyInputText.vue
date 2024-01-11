8137df5c3020e6404f74b5f5db731a8bd8f5caf4
MingGin <84062280+MingGin@users.noreply.github.com>
2024-01-11 09:57:28 +0900

    2024-01-11 최종 수정

--- vuetodo/src/components/MyInputText.vue
+++ vuetodo/src/components/MyInputText.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="button_box div_solid" @focusout="fnInputDetail()">
-        <input type="checkbox" class="form-check-input input_box" v-model="checked"/>
+        <input type="checkbox" class="form-check-input input_box" v-model="checked" @click="checkedEvent"/>
         <input v-model="inputText2" id="inptBox" v-if = "isInputRef" class="form-control maple-light input"  placeholder="오늘의 할일을 입력하세요." v-focus @keyup.enter="fnInputDetail()" /> 
         <text :class="classObject" v-else class="maple-light input" @dblclick="fnTextDbClick()">{{ inputText }}</text>
         <button class="btn btn-danger" v-show="!isInputRef" @click="$emit('removeTodo', itemId)">delete</button>
@@ -9,7 +9,7 @@
 
 <script setup>
 // import MyInput from './MyInput.vue'
-import {ref, reactive} from 'vue';
+import {ref, reactive, watch} from 'vue';
 import {defineProps} from 'vue';
 
     const props = defineProps({                  
@@ -47,30 +47,57 @@ import {defineProps} from 'vue';
         mounted: (el) => el.focus()
     }
 
-    /* text */
+    /* input에 입력된 값의 따라 text 저장*/
     let itemId = ref(props.inpt.id)
     let inputText = JSON.parse(localStorage.getItem(props.inpt.id)).text
+    const label = "오늘의 할일을 입력하세요."
     const inputText2 = ref("")
+        /* 새로고침후 인풋값 안받아와지는 버그 해결 */
+    if(inputText == label){
+        inputText2.value = ""
+    } else{
+        inputText2.value = inputText
+    }
 
     const fnInputText = function() {   
         if(inputText2.value.length > 0){
             inputText = inputText2.value
             const todoItem = {
                 id: props.inpt.id,
-                text: inputText2.value,
-                checked: checked,
+                text: inputText,
+                checked: checked.value,
                 date: new Date().getDate
             }
             localStorage.setItem(todoItem.id,JSON.stringify(todoItem))
+            console.log(1)
         } else{
-            inputText = JSON.parse(localStorage.getItem(props.inpt.id)).text
+            inputText = "오늘의 할일을 입력하세요."
+            const todoItem = {
+                id: props.inpt.id,
+                text: inputText,
+                checked: checked.value,
+                date: new Date().getDate
+            }
+            localStorage.setItem(todoItem.id,JSON.stringify(todoItem))
         }
     }
     /* checkBox */
     const checked = ref(props.inpt.checked)
-    console.log(props.inpt.checked)
+    
     const classObject = reactive({
         text_deco: checked
+    })
+
+    /* checked 속성 localStorage에 변경될때마다 저장 */
+    watch(checked, (newChecked) => {
+        const todoItem = {
+            id: props.inpt.id,
+            text: inputText,
+            checked: newChecked,
+            date: new Date().getDate
+        }
+        console.log(checked.value)
+    localStorage.setItem(todoItem.id,JSON.stringify(todoItem))
     })
 
 </script>
